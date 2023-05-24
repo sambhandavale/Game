4c764c3eb791cf1f2ccd3e40bc35a5b44d71b8a7
sambhandavale <125118342+sambhandavale@users.noreply.github.com>
2023-05-24 23:02:54 +0530

    Update Guess_the_number.py

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -1,6 +1,6 @@
 import random
 print("GUESS the Number Game!!!")
-x = int(input("Guess number between 1 and 6:"))
+x = int(input("Guess number between 1 and 6:\n"))
 a = random.randrange(1, 7)
 if x == a:
     print("CORRECT GUESS!!!")
