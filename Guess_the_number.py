9f232813ac8c8c722f399838db07083426f15457
sambhandavale <125118342+sambhandavale@users.noreply.github.com>
2023-05-24 22:31:58 +0530

    Add files via upload

---
+++ Guess_the_number.py
@@ -0,0 +1,10 @@
+import random
+print("GUESS the Number Game!!!")
+x = int(input("Guess number between 1 and 6:"))
+a = random.randrange(1, 7)
+if x == a:
+    print("CORRECT GUESS!!!")
+
+else:
+    print("WRONG GUESS #####")
+    print("Number was ",a)
